65b58c7f8335c96c6c2b41cc3e1480e985795784
Vijayesh Jeevan <vijayeshjeevan.cs19@bmsce.ac.in>
2020-11-18 19:10:21 +0530

    Offf

--- index/index.js
+++ index/index.js
@@ -1,3 +1,15 @@
+//COde for working of dropdownlist
+
 $(".dropdown-menu li a").click(function () {
   $(".btn:first-child").html($(this).text() + ' <span class="caret"></span>');
 });
+
+//Code for working of the slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
