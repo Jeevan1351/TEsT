6cf26e9503644a87020f6cd376bd93f2edac20dc
Vijayesh Jeevan <vijayeshjeevan.cs19@bmsce.ac.in>
2020-11-18 22:36:50 +0530

    Done

--- index/index.js
+++ index/index.js
@@ -1,15 +1,23 @@
 //COde for working of dropdownlist
 
 $(".dropdown-menu li a").click(function () {
+  console.log(2);
   $(".btn:first-child").html($(this).text() + ' <span class="caret"></span>');
 });
 
-//Code for working of the slider
+//Code for working of the complexity slider
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-  output.innerHTML = this.value;
+const slider_complex = document.getElementById("myRange");
+var output_complex = document.getElementById("demo");
+output_complex.innerHTML = slider_complex.value;
+//Code for Animation speed slider
+slider_complex.oninput = function () {
+  output_complex.innerHTML = this.value;
 };
+
+//Code for Pause button function
+function pause_resume() {
+  var value = document.getElementById("pause").innerHTML;
+  if (value == "Pause") document.getElementById("pause").innerHTML = "Resume";
+  else document.getElementById("pause").innerHTML = "Pause";
+}
